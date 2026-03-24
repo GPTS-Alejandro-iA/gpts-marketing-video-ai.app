2c070517b1c8675b55bba191bbfc725249dbfd8f
Green Power Tech Store LLC <gpts.citas@gmail.com>
2026-03-24 02:27:43 -0400

    Update page.jsx

--- app/generate/page.jsx
+++ app/generate/page.jsx
@@ -1,1 +1,27 @@
+"use client";
+
+import { runVideoBackend } from "@/lib/runpod";
+import { useState } from "react";
+
+export default function GeneratePage() {
+  const [result, setResult] = useState(null);
+
+  async function handleGenerate() {
+    const response = await runVideoBackend("hola");
+    console.log(response);
+    setResult(response);
+  }
+
+  return (
+    <div>
+      <button onClick={handleGenerate}>
+        Generar video
+      </button>
+
+      {result && (
+        <pre>{JSON.stringify(result, null, 2)}</pre>
+      )}
+    </div>
+  );
+}
 
