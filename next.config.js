bd5dd6d7e581a7d8d431baf07f6b487c47a2a932
Green Power Tech Store LLC <gpts.citas@gmail.com>
2026-04-27 23:34:25 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
-  experimental: {
-    appDir: true
-  }
+  reactStrictMode: false
 };
 
 module.exports = nextConfig;
+
 
